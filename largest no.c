149dd758ca14f83e8e12b252f2b8836f0c77c946
bharathibemech <35832761+bharathibemech@users.noreply.github.com>
2018-02-06 16:31:31 +0530

    Create largest no.c

---
+++ largest no.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k;
+    
+    scanf("%d %d %d",&i,&j,&k);
+    
+    if(i>j&&i>k)
+    {
+        printf("%d",i);
+    }
+    else if(b>c&&b>a)
+    {
+        printf("%d",j);
+    }
+    else
+    {
+        printf("%d",k);
+    }
+}
