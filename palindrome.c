c61d23467f6fb8612fa05be0638e22620eceb023
bharathibemech <35832761+bharathibemech@users.noreply.github.com>
2018-02-08 16:19:00 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,i,j,k;
+   scanf("%d",&a);
+   i=a/100;
+   j=(a-(b*100))/10;
+   j=a-((b*100)+(i*10));
+   k=(j*100)+(i*10)+b;
+   if(a==e)
+   printf("IT iS a PALINDROME");
+   else
+   printf("IT IS NOt a PALINDROME");
+   return 0;
+}
