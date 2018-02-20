ede4339363a601b2707684148557f6e6c49cf365
bharathibemech <35832761+bharathibemech@users.noreply.github.com>
2018-02-20 16:46:33 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int b,k,d;
+printf("enter the limit");
+scanf("%d",&k);
+printf("the numbers divisible by %d are\n\n",d);
+{
+for(i=1;i<=b;i++)
+if(i%d==0)
+printf("%d",b);
+}
+getch();
+}
