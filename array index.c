bb03a506bd16f4e3ee878f78a418280d9568a64d
bharathibemech <35832761+bharathibemech@users.noreply.github.com>
2018-03-13 10:49:52 +0000

    Create array index.c

---
+++ array index.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,b[10],k;
+printf("enter no of elements in an array");
+scanf("%d",&k);
+printf("enter the elements in an array");
+for(j=0;j<c;i++)
+{
+scanf("%d\n",&k[j]);
+}
+for(j=0;j<k;j++)
+{
+printf("k[%d]=%d\n",j,k[j]);
+}
+}
